3461230aa6e95512a8567a04f03a50354b946ea9
Yungo <29724679+yungoyungo@users.noreply.github.com>
2020-07-31 11:45:08 +0900

    モデルにテーブル定義を追加

--- app/Tweet.php
+++ app/Tweet.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
+    protected $table = 'tweets';
+    protected $fillable = [
+        'user_id',
+        'tweet',
+    ];
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
