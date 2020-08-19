38053ee64dbbe230a5b9fd39d14732e02761dc18
kyosuke-kkk <kwk0708@icloud.com>
2020-08-19 11:50:40 +0900

    スタイルの当て込み

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,29 @@
 
 @section('content')
 <div class="container">
-<h1>Home</h1>
-{!! Form::open(['action' => 'TweetController@store']) !!}
-        <div class="form-group">
-            {!! Form::label('Tweet', 'Tweet') !!}
-            {!! Form::text('tweet', null, ['class' => 'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::submit('ツイートする', ['class' => 'btn btn-primary form-control']) !!}
-        </div>
-{!! Form::close() !!}
+    <h1>Home</h1>
+        {!! Form::open(['action' => 'TweetController@store']) !!}
+                <div class="form-group">
+                    {!! Form::label('Tweet', 'Tweet') !!}
+                    {!! Form::text('tweet', null, ['class' => 'form-control']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::submit('ツイートする', ['class' => 'btn btn-primary form-control']) !!}
+                </div>
+        {!! Form::close() !!}
 
-@foreach($tweets as $tweet)
-<center>
-    <tr>
-        <pre><td>{{ $tweet->user->name }}</td>     <td>{{ $tweet->created_at }}</td></pre>
-        <td>{{ $tweet->tweet }}</td><br><br>
-    </tr>
-</center>
+    @foreach($tweets as $tweet)
+        <div class="tweet-card">
+            <table>
+                <tr class="tweet-card-header">
+                        <td class="tweet-card-username">{{ $tweet->user->name }}</td>     <td class="tweet-card-date">{{ $tweet->created_at }}</td>
+                </tr>
+                
+                <tr>
+                    <td class="tweet-card-body">{{ $tweet->tweet }}</td><br><br>
+                </tr>
+            </table>
+        </div>
 @endforeach
 </div>
 @endsection
