33765dbdf6beeb057c55ce49a143f17a32b92fef
kyosuke-kkk <kwk0708@icloud.com>
2020-08-22 11:14:29 +0900

    toppageの変更

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('create', 'TweetController@create');
 Route::post('create', 'TweetController@store');
